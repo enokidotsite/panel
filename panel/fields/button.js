96ee033374ca65abee2f6c7c66522aa8df2b08a8
jondashkyle <contact@jon-kyle.com>
2017-08-12 23:03:51 -0400

    removed panel for now and added dev example

--- panel/fields/button.js
+++
@@ -1,9 +0,0 @@
-var html = require('bel')
-
-module.exports = button
-
-function button (state, emit) {
-  return html`
-    <div>${state.value}</div>
-  `
-}
--- panel/fields/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  text: require('./text'),
-  textarea: require('./textarea'),
-  structure: require('./structure'),
-  tags: require('./tags')
-}
