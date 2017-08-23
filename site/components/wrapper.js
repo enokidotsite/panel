f76b1938162129656b4a8376b658c2a18833f843
jondashkyle <contact@jon-kyle.com>
2017-08-23 12:58:41 -0400

    panel flag

--- site/components/wrapper.js
+++ site/components/wrapper.js
@@ -8,6 +8,9 @@ function wrapper (view) {
   return function (state, emit) {
     return html`
       <main>
+        <div class="psf t0 r0 z2">
+          <a class="db bgblack p1 tcwhite" href="?panel=active">Panel</a>
+        </div>
         <div class="c12 p1">
           ${title(state.content)}
           ${navigation({
