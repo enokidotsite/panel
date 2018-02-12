193b0b443632d749c311c01baae34a4d9c2a3c55
jondashkyle <contact@jon-kyle.com>
2018-02-12 13:55:16 -0800

    sites cleanup

--- source/views/sites-create.js
+++ source/views/sites-create.js
@@ -11,9 +11,17 @@ function SitesCreate (state, emit) {
   var designs = objectValues(state.designs.public)
 
   return html`
-    <div class="c12 x xx xw p2 psr">
-      <div class="c12 sm-c6">
-        <div class="w100 psst p1" style="top: 0.75rem; padding-bottom: 8rem">
+    <div class="c12 psr">
+      <div class="px3">
+        <div class="x xw py1 xjb">
+          <div class="fs2 px1 py2 toe wsnw oxh c12 sm-xx fw500">
+            Create a New Site
+          </div>
+        </div>
+        <div class="px1"><div class="bb1-bg10"></div></div>
+      </div>
+      <div class="x xw c12 p2" style="padding-bottom: 8rem">
+        <div class="c12 sm-c6 p1">
           ${Fields({
             blueprint: blueprint,
             draft: { },
@@ -22,19 +30,21 @@ function SitesCreate (state, emit) {
             handleFieldUpdate: handleFieldUpdate
           })}
         </div>
-      </div>
-      <div class="c12 sm-c6 x xw p1" style="padding-bottom: 8rem">
-        <div class="c12 ttu fc-bg25 p1 pt2 fs0-8 x xjb">
-          <div class="fwb">Design</div>
-          <div>More coming soon</div>
+        <div class="c12 sm-c6">
+          <div class="c12 px1 x xjb pt3 usn fs0-8 ttu fc-bg25">
+            <div class="fwb">Design</div>
+            <div>More on the way</div>
+          </div>
+          <div class="c12 x xw">
+            ${designs.map(function (design) {
+              var props = xtend({
+                active: state.sites.create.url === design.url,
+                handleSelect: handleDesignSelect
+              }, design)
+              return renderDesign(props)
+            })}
+          </div>
         </div>
-        ${designs.map(function (design) {
-          var props = xtend({
-            active: state.sites.create.url === design.url,
-            handleSelect: handleDesignSelect
-          }, design)
-          return renderDesign(props)
-        })}
       </div>
       <div class="psf b0 l0 r0 p1 x xjc">
         <div class="p1">
@@ -63,7 +73,6 @@ function SitesCreate (state, emit) {
   }
 
   function handleCreate (event) {
-    console.log(state.sites.createUrl)
     event.preventDefault()
     emit(state.events.SITE_CREATE, {
       url: state.sites.create.url
@@ -73,14 +82,17 @@ function SitesCreate (state, emit) {
 
 function renderDesign (props) {
   return html`
-    <div class="c12 p1" onclick=${handleSelect}>
-      <div class="br2 b1-bg10 curp oh">
+    <div class="c6 p1" onclick=${handleSelect}>
+      <div class="br2 ${props.active ? 'b2-fg' : 'b1-bg10'} curp oh">
         <img src="${props.thumbnail}" class="db">
       </div>
-      <div class="py1 tac fs0-8 ttu">
-        <a href="${props.url}" class="fc-bg25 fch-fg external" target="_blank">${props.title}</a>
+      <div class="py1 fs0-8 ttu">
+        <a
+          href="${props.url}"
+          class="${props.active ? 'fc-fg' : 'fc-bg25'} fch-fg external"
+          target="_blank"
+        >${props.title}</a>
       </div>
-      active: ${props.active === true}
     </div>
   `
 
