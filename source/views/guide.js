18e7374fe947050467cebd64187360ac594139c0
jondashkyle <contact@jon-kyle.com>
2018-02-16 17:28:55 -0800

    cleanup output

--- source/views/guide.js
+++ source/views/guide.js
@@ -5,16 +5,22 @@ var format = require('../components/format')
 module.exports = wrapper(view)
 
 function view (state, emit) {
+  var tags = state.page.tags || [ ]
   return html`
-    <div class="p2">
-      <div class="p2">
+    <div class="p2 x xjc xw">
+      <div class="p2 c12">
         <h2
-          class="fs3 lh1-25"
+          class="fs3 lh1-25 tac"
           style="color: ${state.page.color}"
         >${state.page.title}</h2>
       </div>
-      <div class="p2 x xjc">
-        <div class="copy c12 sm-c10 md-c8 p2">
+      <div class="p2 sm-pt4 c12 tac fs0-8">
+        ${tags.map(function (tag) {
+          return html`<span class="button-inline">${tag}</span>`
+        })}
+      </div>
+      <div class="p2">
+        <div class="copy">
           ${format(state.page.text)}
         </div>
       </div>
--- source/views/guides.js
+++ source/views/guides.js
@@ -12,7 +12,7 @@ function view (state, emit) {
 
   return html`
     <div class="xx x xdc">
-      <div class="p3 x">
+      <div class="p3 x xw">
         ${guides.map(renderGuide)}
       </div>
     </div>
@@ -21,14 +21,13 @@ function view (state, emit) {
 
 function renderGuide (props) {
   return html`
-    <div class="p1 c6 sm-c4">
+    <div class="p1 c6 x ${props.featured ? 'sm-c8' : 'sm-c4'}">
       <a
         href="/#hub${props.url}"
-        class="oh db p2 fc-bg br2"
-        style="background: ${props.color || '#ccc'}"
+        class="w100 oh x xdc xjb p2 bgc-bg2-5 fc-bg25 fch-fg  ${props.featured ? 'fs2' : ''} br2 tfyh"
       >
-        <h2 class="fwb lh1-25 mb2">${props.title}</h2>
-        <div class="copy fs0-8" style="max-height: 5.5rem;">
+        <h2 class="fwb lh1-25 mb2 fc-fg" style="color: ${props.color}">${props.title}</h2>
+        <div class="copy ${props.featured ? 'fs1' : 'fs0-8'}" style="${props.featured ? 'max-height: 10.5rem; max-width: 50rem;' : 'max-height: 5.5rem; max-width: 30rem;'}">
           <p>${props.excerpt}</p>
         </div>
       </a>
