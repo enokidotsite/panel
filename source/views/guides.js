c0c46ad7f457040710c14f110f7bdf9f9583520b
Jon-Kyle Mohr <contact@jon-kyle.com>
2018-03-13 16:34:14 -0700

    nice

--- source/views/guides.js
+++ source/views/guides.js
@@ -8,9 +8,9 @@ var wrapper = require('../containers/wrapper-hub')
 module.exports = wrapper(view)
 
 function view (state, emit) {
-  var guides = objectValues(state.page.pages).map(function (page){
-    return state.docs.content[page.url] || { }
-  })
+  var guides = objectValues(state.page.pages)
+    .map(page => state.docs.content[page.url])
+    .filter(page => (page && page.visible !== false))
 
   return html`
     <div class="xx bgc-bg2-5">
